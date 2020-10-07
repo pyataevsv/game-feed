092c2b220c92f1958d051ec36df38231e07fc459
pyataevsv <pyataevsv@gmail.com>
2020-10-07 11:58:15 +0300

    flex

--- .next/server/pages/game/[slug].js
+++ .next/server/pages/game/[slug].js
@@ -1,7 +1,7 @@
 module.exports =
 /******/ (function(modules) { // webpackBootstrap
 /******/ 	// The module cache
-/******/ 	var installedModules = require('../ssr-module-cache.js');
+/******/ 	var installedModules = require('../../ssr-module-cache.js');
 /******/
 /******/ 	// The require function
 /******/ 	function __webpack_require__(moduleId) {
@@ -88,16 +88,188 @@ module.exports =
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = 0);
+/******/ 	return __webpack_require__(__webpack_require__.s = 1);
 /******/ })
 /************************************************************************/
 /******/ ({
 
-/***/ 0:
+/***/ 1:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__("IlR1");
+module.exports = __webpack_require__("2I6W");
 
+
+/***/ }),
+
+/***/ "2I6W":
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+// ESM COMPAT FLAG
+__webpack_require__.r(__webpack_exports__);
+
+// EXPORTS
+__webpack_require__.d(__webpack_exports__, "getServerSideProps", function() { return /* binding */ getServerSideProps; });
+
+// EXTERNAL MODULE: external "react"
+var external_react_ = __webpack_require__("cDcd");
+var external_react_default = /*#__PURE__*/__webpack_require__.n(external_react_);
+
+// EXTERNAL MODULE: external "next/router"
+var router_ = __webpack_require__("4Q3z");
+
+// EXTERNAL MODULE: external "styled-components"
+var external_styled_components_ = __webpack_require__("Dtiu");
+var external_styled_components_default = /*#__PURE__*/__webpack_require__.n(external_styled_components_);
+
+// EXTERNAL MODULE: ./src/redux/store.js + 1 modules
+var store = __webpack_require__("fTHX");
+
+// CONCATENATED MODULE: ./src/components/Slider.js
+var __jsx = external_react_default.a.createElement;
+
+
+const Wrapper = external_styled_components_default.a.div.withConfig({
+  displayName: "Slider__Wrapper",
+  componentId: "sc-17tqp4z-0"
+})(["float:left;display:flex;width:60%;@media (max-width:1189px){width:100%;}button{display:block;width:30px;font-size:20px;font-weight:boler;color:grey;outline-style:none;border:none;background-color:#333;&:hover{cursor:pointer;background-color:#444;}&:active{background-color:#555;}}"]);
+const Sliderbox = external_styled_components_default.a.div.withConfig({
+  displayName: "Slider__Sliderbox",
+  componentId: "sc-17tqp4z-1"
+})(["border-radius:5px;width:100%;padding:25% 0;position:relative;overflow:hidden;flex:1;"]);
+const Innerbox = external_styled_components_default.a.div.withConfig({
+  displayName: "Slider__Innerbox",
+  componentId: "sc-17tqp4z-2"
+})(["position:relative;width:100%;height:100%;right:40px;"]);
+const Img = external_styled_components_default.a.div.withConfig({
+  displayName: "Slider__Img",
+  componentId: "sc-17tqp4z-3"
+})(["position:absolute;top:0;left:", ";background-image:url(", ");background-size:cover;background-position:center;width:100%;height:100%;transition:all .5s ease-in-out;"], props => props.id * 100 - 100 * props.state + '%', props => props.src);
+
+function Slider({
+  slides
+}) {
+  const {
+    0: state,
+    1: setState
+  } = Object(external_react_["useState"])(1);
+  console.log(slides);
+  return __jsx(Wrapper, null, __jsx("button", {
+    onClick: () => {
+      if (state != 1) setState(state - 1);
+    }
+  }, "\u2190"), __jsx(Sliderbox, null, slides ? slides.map((i, id) => __jsx(Img, {
+    key: id,
+    id: id,
+    src: i.image,
+    state: state
+  })) : null), __jsx("button", {
+    onClick: () => {
+      if (state != slides.length - 1) setState(state + 1);
+    }
+  }, "\u2192"));
+}
+
+/* harmony default export */ var components_Slider = (Slider);
+// CONCATENATED MODULE: ./src/pages/game/[slug].js
+var _slug_jsx = external_react_default.a.createElement;
+
+
+
+
+
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+const Column = external_styled_components_default.a.div.withConfig({
+  displayName: "slug__Column",
+  componentId: "y6zr3n-0"
+})(["box-sizing:border-box;float:left;width:40%;padding:0px 20px;@media (max-width:1189px){width:100%;padding:0px 0px;}a{color:orange;text-decoration:none;line-height:40px;font-weight:bolder;}"]);
+const Head = external_styled_components_default.a.div.withConfig({
+  displayName: "slug__Head",
+  componentId: "y6zr3n-1"
+})(["display:flex;justify-content:space-between;align-items:center;padding:10px 0px;div:nth-child(1){font-size:33px;}div:nth-child(2){font-size:24px;padding:5px 10px;background-color:#444;border-radius:5px;border:1px solid #777;}"]);
+const Released = external_styled_components_default.a.div.withConfig({
+  displayName: "slug__Released",
+  componentId: "y6zr3n-2"
+})(["padding:0px 0px;text-align:right;color:#777;"]);
+const Descr = external_styled_components_default.a.div.withConfig({
+  displayName: "slug__Descr",
+  componentId: "y6zr3n-3"
+})(["color:#e5e4e4;letter-spacing:0.3px;position:relative;overflow:hidden;height:", ";div{position:", ";top:0;left:0;width:100%;height:102%;text-align:justify;}button{display:block;position:absolute;bottom:0px;width:100%;background-color:rgba(50,50,50,0.95);color:white;border:none;outline-style:none;padding:5px;cursor:pointer;}"], props => props.open ? null : '100px', props => props.open ? 'relative' : 'absolute');
+
+const Game = () => {
+  const {
+    0: descr,
+    1: setDescr
+  } = Object(external_react_["useState"])(false);
+  const {
+    0: slides,
+    1: setSlides
+  } = Object(external_react_["useState"])(null);
+  const {
+    0: {
+      name,
+      description_raw,
+      website,
+      background_image,
+      rating,
+      released
+    },
+    1: setDetails
+  } = Object(external_react_["useState"])({});
+  const router = Object(router_["useRouter"])();
+  const {
+    slug
+  } = router.query; //screenshots
+
+  Object(external_react_["useEffect"])(() => {
+    if (slug) {
+      const scrURL = 'https://api.rawg.io/api/games/' + slug + '/screenshots';
+      fetch(scrURL).then(x => {
+        if (!x.ok) throw new Error(x); //console.log(x)
+
+        return x.json();
+      }).then(x => setSlides(x.results)).catch(x => console.log(x));
+      return () => {};
+    }
+  }, []); //screenshots
+
+  Object(external_react_["useEffect"])(() => {
+    if (slug) {
+      const scrURL = 'https://api.rawg.io/api/games/' + slug;
+      fetch(scrURL).then(x => {
+        if (!x.ok) throw new Error(x);
+        console.log(x);
+        return x.json();
+      }).then(x => {
+        console.log(x);
+        setDetails(x);
+      }).catch(x => console.log(x));
+      return () => {};
+    }
+  }, []);
+  return _slug_jsx("div", null, _slug_jsx(components_Slider, {
+    slides: slides
+  }), _slug_jsx(Column, null, _slug_jsx(Head, null, _slug_jsx("div", null, name), _slug_jsx("div", null, rating)), _slug_jsx(Released, null, new Date(released).getDate(), "  ", monthNames[new Date(released).getMonth()], ", ", new Date(released).getFullYear()), _slug_jsx(Descr, {
+    open: descr
+  }, _slug_jsx("div", null, description_raw, _slug_jsx("br", null), "\xA0", _slug_jsx("br", null), "\xA0"), _slug_jsx("button", {
+    onClick: () => setDescr(!descr)
+  }, descr ? 'less' : 'more')), _slug_jsx("a", {
+    href: website,
+    target: '_blank'
+  }, "Go to website \u2192")));
+};
+
+const getServerSideProps = async ({
+  params
+}) => {
+  const slug = params.slug;
+  return {
+    props: {
+      slug: params.slug
+    }
+  };
+};
+/* harmony default export */ var _slug_ = __webpack_exports__["default"] = (Game);
 
 /***/ }),
 
@@ -108,49 +280,17 @@ module.exports = require("redux-logger");
 
 /***/ }),
 
+/***/ "4Q3z":
+/***/ (function(module, exports) {
+
+module.exports = require("next/router");
+
+/***/ }),
+
 /***/ "Dtiu":
 /***/ (function(module, exports) {
 
 module.exports = require("styled-components");
-
-/***/ }),
-
-/***/ "IlR1":
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, "default", function() { return App; });
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__("cDcd");
-/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__("h74D");
-/* harmony import */ var react_redux__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react_redux__WEBPACK_IMPORTED_MODULE_1__);
-/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__("Dtiu");
-/* harmony import */ var styled_components__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(styled_components__WEBPACK_IMPORTED_MODULE_2__);
-/* harmony import */ var _redux_store__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__("fTHX");
-
-var __jsx = react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement;
-
-
-
-
-const GlobalStyle = styled_components__WEBPACK_IMPORTED_MODULE_2__["createGlobalStyle"]`
-  body {
-    font-family:Helvetica, sans-serif;
-    margin:0px;
-    padding: 20px;
-    background-color: #151515;
-    color: whitesmoke;
-  }
-`;
-function App({
-  Component,
-  pageProps
-}) {
-  return __jsx(react_redux__WEBPACK_IMPORTED_MODULE_1__["Provider"], {
-    store: _redux_store__WEBPACK_IMPORTED_MODULE_3__[/* default */ "a"]
-  }, __jsx(GlobalStyle, null), __jsx(Component, pageProps));
-}
 
 /***/ }),
 
@@ -404,13 +544,6 @@ function fetchingErrorReducer(state = iniState.fetchingError, action) {
 
 const store = Object(external_redux_["createStore"])(reducers, Object(external_redux_["applyMiddleware"])(external_redux_thunk_default.a, external_redux_logger_default.a));
 /* harmony default export */ var redux_store = __webpack_exports__["a"] = (store);
-
-/***/ }),
-
-/***/ "h74D":
-/***/ (function(module, exports) {
-
-module.exports = require("react-redux");
 
 /***/ }),
 
